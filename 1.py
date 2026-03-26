95c4b5feeb98e23cc480ebb7e7bf8392773d1cf4
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-26 16:54:43 +0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+string = input("please enter your own word :   ")
+
+char = input("enter your own character :   ")
+i = 0
+count = 0
+
+while(i < len(string)):
+
+     if(string[i] == char):
+        count = count + 1
+     i = i + 1
+
+print("The total number of times", char, "has Occurred = ", count)
